3ddb7d6962d5a3c05d53b40e9a0417a0cbe521a5
Xaero Yuan <xaeroyuan@gmail.com>
2015-08-23 00:21:14 +1000

    update config

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -106,7 +106,7 @@ flags = [
 #
 # Most projects will NOT need to set this to anything; you can just change the
 # 'flags' list of compilation flags. Notice that YCM itself uses that approach.
-compilation_database_folder = '/Users/xaero/workspace/TestQt'
+compilation_database_folder = '/Users/xaero/workspace/Study-QT5'
 
 if os.path.exists( compilation_database_folder ):
   database = ycm_core.CompilationDatabase( compilation_database_folder )
